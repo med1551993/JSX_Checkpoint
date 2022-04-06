08d8d51224c6eefdffe148030801f7d56598a356
med1551993 <jradi93@gmail.com>
2022-04-06 23:46:41 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import './style.css';
+import myWonderfulImage from "./imageInSrc.jpg"
+function App() {
+  return (
+    <div className="App">
+
+        <div style={{border: "solid 1px black", maxWidth:"100vw"}}>
+
+            <h1 className="title red">Your name here</h1>
+            <br/>
+            <img src={myWonderfulImage} alt ='myImage'/>
+            <br/>
+            <img src="/imageInPublic.jpg" alt="cat"/>
+
+        </div>
+
+        <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+        </video>
+
+    </div>
+  );
+}
+
+export default App;
